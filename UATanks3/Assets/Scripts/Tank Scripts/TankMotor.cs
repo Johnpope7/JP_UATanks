94f65441e90b876acc17c8164765e5cd8a3d0df9
Johnpope7 <Johnpope5387@gmail.com>
2021-07-08 06:33:02 -0700

    Added Tank Motor basics
    added the basic functions for the Tank Motor script

---
+++ UATanks3/Assets/Scripts/Tank Scripts/TankMotor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMotor : MonoBehaviour
+{
+    #region Variables
+    private CharacterController m_characterController;
+
+    #endregion
+
+    #region default methods
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region custom methods
+    public void Move(float moveSpeed) 
+    {
+        Vector3 movementVector = transform.forward * moveSpeed;
+        m_characterController.SimpleMove(movementVector);
+    }
+
+    public void Rotate(float rotateSpeed) 
+    {
+        Vector3 rotationVector = transform.up * rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotationVector);
+    }
+    #endregion
+}
