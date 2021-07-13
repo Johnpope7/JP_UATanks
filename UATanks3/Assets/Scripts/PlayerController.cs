c2454db88480fee6c11f9e7331a0e6bb156f5200
Johnpope7 <Johnpope5387@gmail.com>
2021-07-13 15:48:13 -0700

    Finished Basic Player Controller
    Completed all steps needed to make a basic functioning player controller, and commented out the tank shooter script to begin work on it later

--- UATanks3/Assets/Scripts/PlayerController.cs
+++ UATanks3/Assets/Scripts/PlayerController.cs
@@ -9,36 +9,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     #region Variables
-    private TankData m_data;
-    private TankMotor m_motor;
-    private TankShooter m_shooter;
-    public enum InputScheme { WASD, arrowKeys };
-    public InputScheme m_playerInputScheme = InputScheme.WASD;
+    private TankData m_data; //variable for storing the required Tank Data component
+    private TankMotor m_motor; //variable for storing the required Tank Motor component
+    private TankShooter m_shooter; //variable for storing the required Tank Shooter component
+    public enum InputScheme { WASD, arrowKeys }; //stores two input schemes, for multiplayer
+    public InputScheme m_playerInputScheme = InputScheme.WASD; //sets the input scheme to WASD for now
     #endregion
 
     #region default methods
     // Start is called before the first frame update
    private void Start()
     {
-        m_data = gameObject.GetComponent<TankData>();
-       m_motor = gameObject.GetComponent<TankMotor>();
-        m_shooter = gameObject.GetComponent<TankShooter>();
+        m_data = gameObject.GetComponent<TankData>(); //sets the proper Tank Data script for the player
+       m_motor = gameObject.GetComponent<TankMotor>(); //sets the proper Tank Motor script for the player
+        m_shooter = gameObject.GetComponent<TankShooter>(); //sets the proper Tank Shooter script for the player
     }
 
     // Update is called once per frame
     private void Update()
     {
-        switch (m_playerInputScheme)
+        switch (m_playerInputScheme) //This switch statement handles movement, mainly the input schemes for WASD and the Arrow Keys
         {
-            case InputScheme.WASD:
+            case InputScheme.WASD: //case for WASD controls
+                //handles movement speed
                 if (Input.GetKey(KeyCode.W))
                 {
                     m_motor.Move(m_data.moveSpeed);
                 }
-                if (Input.GetKey(KeyCode.S))
+                else if (Input.GetKey(KeyCode.S))
                 {
                     m_motor.Move(-m_data.moveSpeed);
                 }
+                else //need to pass in a movement speed of 0 to ensure gravity is being used
+                {
+                    m_motor.Move(0f); 
+                }
+                //handles rotation
                 if (Input.GetKey(KeyCode.A))
                 {
                     m_motor.Rotate(-m_data.rotateSpeed);
@@ -47,20 +53,27 @@ public class PlayerController : MonoBehaviour
                 {
                     m_motor.Rotate(m_data.rotateSpeed);
                 }
+                //handles the tanks attack, shooting
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     m_shooter.Shoot(); ;
                 }
                 break;
-            case InputScheme.arrowKeys:
+            case InputScheme.arrowKeys: //case for Arrow Keys Controls
+                //handles movement speed
                 if (Input.GetKey(KeyCode.UpArrow))
                 {
                     m_motor.Move(m_data.moveSpeed);
                 }
-                if (Input.GetKey(KeyCode.DownArrow))
+                else if (Input.GetKey(KeyCode.DownArrow))
                 {
                     m_motor.Move(m_data.moveSpeed);
                 }
+                else //need to pass in a movement speed of 0 to ensure gravity is being used
+                {
+                    m_motor.Move(0f);
+                }
+                //handles rotation
                 if (Input.GetKey(KeyCode.LeftArrow))
                 {
                     m_motor.Rotate(-m_data.rotateSpeed);
@@ -69,13 +82,14 @@ public class PlayerController : MonoBehaviour
                 {
                     m_motor.Rotate(m_data.rotateSpeed);
                 }
+                //handles the tanks attack, shooting
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    m_shooter.Shoot(); ;
+                    m_shooter.Shoot();
                 }
                 break;
             default:
-                Debug.LogWarning("[PlayerController1] Unimplemented Input Scheme used.");
+                Debug.LogWarning("[PlayerController1] Unimplemented Input Scheme used."); //default debug output if WASD or Arrow Keys arent found
                 break;
 
         }
--- UATanks3/Assets/Scripts/Tank Scripts/TankShooter.cs
+++ UATanks3/Assets/Scripts/Tank Scripts/TankShooter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TankShooter : MonoBehaviour
 {
     #region Variables
+    public GameObject m_tankBulletPrefab; //stores the prefab for the tank bullet, may move to the Tank Data script
     #endregion
 
     #region default methods
@@ -24,7 +25,10 @@ public class TankShooter : MonoBehaviour
     #region custom methods
     public void Shoot() 
     {
+        //TODO: Cooldown Timer on Shooting
         Debug.Log("Shots Fired");
+        //TODO: Instantiate a tank bullet
+        //TODO: Add force to the tank bullet
     }
     #endregion
 }
